e53602896f802bbe40ed7218cd7cc558bc24529f
jafet <20220188@ricaldone.edu.sv>
2024-03-17 21:33:54 -0600

    edits variados

--- resources/js/popupLogReg.js
+++ resources/js/popupLogReg.js
@@ -1,7 +1,11 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
