74392317f4c9ca861a35ea90e3333b60ca41d704
bubbleais <dominicasielmejiamendoza2020@gmail.com>
2024-05-12 17:48:19 -0600

    cambios

--- controllers/admin/js/clientes.js
+++ controllers/admin/js/clientes.js
@@ -1,61 +1,178 @@
-const SAVE_MODAL =  new bootstrap.Modal('#CreateModal'),
-MODAL_TITLE = document.getElementById('modalTitle');
+// Constante para completar la ruta de la API.
+const ADMINISTRADOR_API = 'services/admin/clientes.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer los elementos de la tabla.
+const TABLE_BODY = document.getElementById('tableBody'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_CLIENTE = document.getElementById('idCliente'),
+    NOMBRE_CLIENTE = document.getElementById('nombreCliente'),
+    TELEFONO_CLIENTE = document.getElementById('TelefonoCliente'),
+    CORREO_CLIENTE = document.getElementById('CorreoCliente'),
+    ALIAS_CLIENTE = document.getElementById('AliasCliente'),
+    DIRECCION_CLIENTE = document.getElementById('ClienteDireccion'),
+    CLAVE_CLIENTE = document.getElementById('claveCliente'),
+    CONFIRMAR_CLAVE = document.getElementById('confirmarClave');
 
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Gestionar administradores';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
+
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_CLIENTE.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(ADMINISTRADOR_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+const fillTable = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND.textContent = '';
+    TABLE_BODY.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(ADMINISTRADOR_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros fila por fila.
+        DATA.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY.innerHTML += `
+                <tr>
+                    <td>${row.nombre_ciente}</td>
+                    <td>${row.telefono_cliente}</td>
+                    <td>${row.correo_cliente}</td>
+                    <td>${row.alias_cliente}</td>
+                    <td>
+                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_cliente})">
+                        <i class="fa-solid fa-pencil"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_cliente})">
+                        <i class="fa-regular fa-trash-can"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
 const openCreate = () => {
     // Se muestra la caja de diálogo con su título.
     SAVE_MODAL.show();
-    MODAL_TITLE.textContent = 'Crear cliente';
+    MODAL_TITLE.textContent = 'Crear administrador';
     // Se prepara el formulario.
     SAVE_FORM.reset();
+    ALIAS_CLIENTE.disabled = false;
+    CLAVE_CLIENTE.disabled = false;
+    CONFIRMAR_CLAVE.disabled = false;
 }
 
-document.querySelectorAll('.remove-button').forEach(button => {
-    button.addEventListener('click', function () {
-        // Muestra la alerta de SweetAlert2
-        Swal.fire({
-            title: "¿Estás seguro?",
-            text: "¡No podrás revertir esto!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Sí, desactivarlo"
-        }).then((result) => {
-            // Si el usuario confirma, muestra una alerta de éxito
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: "Desactivado!",
-                    text: "El usuario ha sido desactivado.",
-                    icon: "success"
-                });
-                // Aquí puedes agregar la lógica para eliminar el registro
-            }
-        });
-    });
-});
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idAdministrador', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(ADMINISTRADOR_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar administrador';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        ALIAS_CLIENTE.disabled = true;
+        CLAVE_CLIENTE.disabled = true;
+        CONFIRMAR_CLAVE.disabled = true;
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_CLIENTE.value = ROW.id_cliente;
+        NOMBRE_CLIENTE.value = ROW.nombre_ciente;
+        TELEFONO_CLIENTE.value = ROW.telefono_cliente;
+        CORREO_CLIENTE.value = ROW.correo_cliente;
+        DIRECCION_CLIENTE.value = ROW.direccion_cliente;
+        ALIAS_CLIENTE.value = ROW.alias_cliente;
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
 
-$('.edit-button').click(function () {
-    // Abre el modal al hacer clic en el botón de editar
-    $('#exampleModal1').modal('show');
-
-    document.getElementById("editForm").addEventListener("submit", function (event) {
-        // Prevenimos el comportamiento por defecto (envío del formulario)
-        event.preventDefault();
-
-        // Simulamos un guardado exitoso
-        // Aquí podrías realizar una llamada Ajax para guardar los datos
-
-        // Mostramos la alerta de SweetAlert
-        Swal.fire({
-            icon: "success",
-            text: "Actualizado exitosamente"
-
-        });
-
-        // Cerramos el modal
-        $('#exampleModal1').modal('hide')
-
-    });
-});
-
-
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el administrador de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idAdministrador', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(ADMINISTRADOR_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
