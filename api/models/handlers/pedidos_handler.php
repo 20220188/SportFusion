d02c0efa335e3c0533e3a513a7784c9a29a3a921
jafet <20220188@ricaldone.edu.sv>
2024-05-14 13:31:31 -0600

    pedidos iniciado

---
+++ api/models/handlers/pedidos_handler.php
@@ -0,0 +1,154 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el comportamiento de los datos de la tabla PRODUCTO.
+*/
+class PedidoHandler
+{
+    /*
+    *   Declaración de atributos para el manejo de datos.
+    */
+
+    // Atributos de la tabla PEDIDO.
+    protected $id_pedido = null;
+    protected $direccion_pedido = null;
+    protected $fecha_registro = null;
+    protected $id_cliente = null;
+    // Atributos de la tabla DETALLE_PEDIDO.
+    protected $id_detalle = null;
+    protected $cantidad_pedido = null;
+    protected $precio_pedido = null;
+    protected $id_pedido = null;
+    protected $id_producto = null;
+    protected $id_estado_pedido = null;
+
+    
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD en tabla PEDIDO (search, create, read, update, and delete).
+    */
+    public function searchRows()
+    {
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_pedido, direccion_pedido, fecha_registro, id_cliente
+                FROM tb_pedidos
+                INNER JOIN tb_clientes USING(id_cliente)
+                WHERE direccion_pedido LIKE ? OR fecha_registro LIKE ? OR id_cliente LIKE ? 
+                ORDER BY direccion_pedido';
+        $params = array($value, $value, $value, $value, $value);
+        return Database::getRows($sql, $params);
+    }
+
+        public function createRow()
+        {
+            $sql = 'INSERT INTO tb_pedidos(direccion_pedido, fecha_registro, id_cliente)
+                    VALUES(?, ?, ?)';
+            $params = array($this->direccion, $this->fecga, $this->cliente);
+            //esto funciona para ver los valores que toma el arreglo print_r($params);
+            return Database::executeRow($sql, $params);
+        }
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_pedido, direccion_pedido, fecha_registro, id_cliente  
+                FROM tb_pedidos
+                INNER JOIN tb_clientes USING(id_cliente)
+                ORDER BY direccion_pedido';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_pedido, direccion_pedido, direccion_pedido, id_cliente
+                FROM tb_pedidos 
+                WHERE id_pedido = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE tb_pedidos p
+                INNER JOIN tb_detalle_pedidos dp USING(id_pedido)
+                SET  p.direccion_pedido = ?, p.direccion_pedido = ?, dp.id_cliente = ?
+                WHERE id_pedido = ?';
+                $params = array($this->direccion, $this->fecga, $this->cliente);        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM tb_pedidos
+                WHERE id_pedido = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD en tabla DETALLE?PRODUCTO (search, create, read, update, and delete).
+    */
+    public function createRow_detalleProducto()
+        {
+            $sql = 'INSERT INTO tb_detalle_pedidos(cantidad_pedido, precio_pedido, id_pedido, id_producto, id_estado_pedido)
+                    VALUES(?, ?, ?, ?, ?)';
+            $params = array($this->cantidad, $this->precio, $this->id_pedido, $this->id_producto, $this->id_estado_pedido);
+            return Database::executeRow($sql, $params);
+        }
+
+        public function readAll_detalle()
+    {
+        $sql = 'SELECT dp.id_detalle, dp.cantidad_pedido, dp.precio_pedido, t.pedido, g.producto, p.estado_pedido
+        FROM tb_detalle_pedidos dp
+        INNER JOIN tb_pedidos t USING(id_pedido)
+        INNER JOIN tb_productos g USING(id_producto)
+        INNER JOIN tb_estado_pedidos p USING(id_estado_pedido)
+        ORDER BY nombre_producto';
+        return Database::getRows($sql);
+    }
+
+    public function readOne_detalle()
+    {
+        $sql = 'SELECT id_detalle, cantidad_pedido, precio_pedido, id_pedido, id_producto, id_estado_pedido
+                FROM tb_detalle
+                INNER JOIN tb_pedidos USING(id_pedido) 
+                WHERE id_detalle = ?';
+        $params = array($this->id_detalle);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow_detalle()
+    {
+        $sql = 'UPDATE tb_detalle 
+                SET cantidad_pedido = ?, precio_pedido = ?, id_pedido = ?, id_producto = ?
+                WHERE id_detalle = ?';
+        $params = array($this->cantidad, $this->precio,$this->id_pedido,$this->id_producto, $this->id_detalle);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow_detalle()
+    {
+        $sql = 'DELETE FROM tb_detalle
+                WHERE id_detalle = ?';
+        $params = array($this->id_detalle);
+        return Database::executeRow($sql, $params);
+    }
+
+    /*
+    *   Métodos para generar reportes.
+    */
+    /*
+    // Método para obtener los productos de una categoría. Comentada porque no se implementan reportes aun
+    public function productosCategoria()
+    {
+        $sql = 'SELECT nombre_producto, precio_producto, estado_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE id_categoria = ?
+                ORDER BY nombre_producto';
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
+    }
+    */
+    
+}
--- controllers/admin/js/pedidos.js
+++ controllers/admin/js/pedidos.js
@@ -1,62 +1,363 @@
-const SAVE_MODAL =  new bootstrap.Modal('#CreateModal'),
-MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para completar las rutas de la API de PRODUCTO.
+const PRODUCTO_API = 'services/admin/producto.php';
+const CATEGORIA_API = 'services/admin/categoria.php';
+const TIPO_PRODUCTO_API = 'services/admin/tipo_producto.php';
+const DEPORTE_API = 'services/admin/deporte.php';
 
+// Constantes para completar las rutas de la API de DETALLE_PRODUCTO.
+const GENERO_API = 'services/admin/genero.php';
+const TALLA_API = 'services/admin/talla.php';
+/*
+*Elementos para la tabla PRODUCTOS
+*/
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer el contenido de la tabla.
+const TABLE_BODY = document.getElementById('tableBody'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_PRODUCTO = document.getElementById('idProducto'),
+    NOMBRE_PRODUCTO = document.getElementById('nombreProducto'),
+    DESCRIPCION_PRODUCTO = document.getElementById('descripcionProducto')
+
+/*
+*Elementos para la tabla DETALLE_PRODUCTO
+*/
+
+// Constantes para establecer el contenido de la tabla.
+const TABLE_BODY_DETALLE = document.getElementById('tableBodyDetalle'),
+    ROWS_FOUND_DETALLE = document.getElementById('rowsFoundDetalle');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL_DETALLE = new bootstrap.Modal('#saveModalDetalle'),
+    MODAL_TITLE_DETALLE = document.getElementById('modalTitleDetalle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM_DETALLE = document.getElementById('saveFormDetalle'),
+    ID_DETALLE = document.getElementById('idDetalle'),
+    PRECIO_DETALLE = document.getElementById('precioDetalle'),
+    EXISTENCIAS_DETALLE = document.getElementById('existenciasDetalle')
+    ID_PRODUCTO_DETALLE = document.getElementById('idProductoDetalle');
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Gestionar pedidos';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
+
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_PRODUCTO.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(PRODUCTO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+const fillTable = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND.textContent = '';
+    TABLE_BODY.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(PRODUCTO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY.innerHTML += `
+                <tr>
+                    <td>${row.nombre_producto}</td>
+                    <td>${row.nombre_categoria}</td>
+                    <td>${row.tipo_producto}</td>
+                    <td>${row.nombre_deporte}</td>
+                    <td>
+                        <button type="button" class="btn btn-success" onclick="openDetails(${row.id_producto})">
+                        <i class="fa-regular fa-square-plus"></i>
+                        </button>
+                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_producto})">
+                        <i class="fa-solid fa-pencil"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_producto})">
+                        <i class="fa-regular fa-trash-can"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
 const openCreate = () => {
     // Se muestra la caja de diálogo con su título.
     SAVE_MODAL.show();
-    MODAL_TITLE.textContent = 'Ingresar pedido';
+    MODAL_TITLE.textContent = 'Crear producto';
     // Se prepara el formulario.
     SAVE_FORM.reset();
+    fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto');
+    fillSelect(TIPO_PRODUCTO_API, 'readAll', 'tipoProducto');
+    fillSelect(DEPORTE_API, 'readAll', 'deporteProducto');
 }
 
-document.querySelectorAll('.remove-button').forEach(button => {
-    button.addEventListener('click', function () {
-        // Muestra la alerta de SweetAlert2
-        Swal.fire({
-            title: "¿Estás seguro?",
-            text: "¡No podrás revertir esto!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Sí, eliminarlo"
-        }).then((result) => {
-            // Si el usuario confirma, muestra una alerta de éxito
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: "¡Eliminado!",
-                    text: "Tu archivo ha sido eliminado.",
-                    icon: "success"
-                });
-                // Aquí puedes agregar la lógica para eliminar el registro
-            }
-        });
-    });
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    // Se define un objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idProducto', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(PRODUCTO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar producto';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_PRODUCTO.value = ROW.id_producto;
+        NOMBRE_PRODUCTO.value = ROW.nombre_producto;
+        DESCRIPCION_PRODUCTO.value = ROW.descripcion;
+        fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto', ROW.id_categoria);
+        fillSelect(TIPO_PRODUCTO_API, 'readAll', 'tipoProducto', ROW.id_tipo_producto);
+        fillSelect(DEPORTE_API, 'readAll', 'deporteProducto', ROW.id_deporte);
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el producto de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idProducto', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(PRODUCTO_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
+
+
+/*
+*   Funciones para la tabla DETALLE_PRODUCTO
+*/
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM_DETALLE.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_DETALLE.value) ? action = 'updateRow_detalleProducto' : action = 'createRow_detalleProducto';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM_DETALLE);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(PRODUCTO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL_DETALLE.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
 });
 
-$('.edit-button').click(function () {
-    // Abre el modal al hacer clic en el botón de editar
-    $('#exampleModal1').modal('show');
-
-    document.getElementById("editForm").addEventListener("submit", function (event) {
-        // Prevenimos el comportamiento por defecto (envío del formulario)
-        event.preventDefault();
-
-        // Simulamos un guardado exitoso
-        // Aquí podrías realizar una llamada Ajax para guardar los datos
-
-        // Mostramos la alerta de SweetAlert
-        Swal.fire({
-            icon: "success",
-            text: "Actualizado exitosamente"
-
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+const fillTableDetails = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND_DETALLE.textContent = '';
+    TABLE_BODY_DETALLE.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll_detalle';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(PRODUCTO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY_DETALLE.innerHTML += `
+                <tr>
+                    <td>${row.precio}</td>
+                    <td>${row.cantidad_disponible}</td>
+                    <td>${row.talla}</td>
+                    <td>${row.genero}</td>
+                    <td>${row.nombre_producto}</td>
+                    <td>
+                        <button type="button" class="btn btn-info" onclick="openUpdateDetails(${row.id_detalle_producto})">
+                        <i class="fa-solid fa-pencil"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger" onclick="openDeleteDetails(${row.id_detalle_producto})">
+                        <i class="fa-regular fa-trash-can"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
         });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND_DETALLE.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
 
-        // Cerramos el modal
-        $('#exampleModal1').modal('hide')
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openDetails = (id_producto) => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL_DETALLE.show();
+    MODAL_TITLE_DETALLE.textContent = 'Crear Detalle producto';
+    // Se prepara el formulario.
+    SAVE_FORM_DETALLE.reset();
 
-    });
-});
+    ID_PRODUCTO_DETALLE.value = id_producto;
+    fillSelect(TALLA_API, 'readAll', 'tallaDetalle');
+    fillSelect(GENERO_API, 'readAll', 'generoDetalle');
 
+    fillTableDetails();
+}
 
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdateDetails = async (id1) => {
+    // Se define un objeto con los datos del registro seleccionado.
+    const FORM_DETALLE = new FormData();
+    FORM_DETALLE.append('idDetalle', id1);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(PRODUCTO_API, 'readOne_detalleProducto', FORM_DETALLE);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL_DETALLE.show();
+        MODAL_TITLE_DETALLE.textContent = 'Actualizar detalle producto';
+        // Se prepara el formulario.
+        SAVE_FORM_DETALLE.reset();
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_DETALLE.value = ROW.id_detalle_producto;
+        PRECIO_DETALLE.value = ROW.precio;
+        EXISTENCIAS_DETALLE.value = ROW.cantidad_disponible;
+        fillSelect(TALLA_API, 'readAll', 'tallaDetalle', ROW.id_talla);
+        fillSelect(GENERO_API, 'readAll', 'generoDetalle', ROW.id_genero);
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
 
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDeleteDetails = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el producto de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idDetalle', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(PRODUCTO_API, 'deleteRow_detalleProducto', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTableDetails();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
+
+/*
+*   Función para abrir un reporte automático de productos por categoría.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openReport = () => {
+    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
+    const PATH = new URL(`${SERVER_URL}reports/admin/productos.php`);
+    // Se abre el reporte en una nueva pestaña.
+    window.open(PATH.href);
+}
